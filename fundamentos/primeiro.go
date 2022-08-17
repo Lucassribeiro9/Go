9e81d9e4330841ce8a6118be452aadfa37edb6a4
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-08-17 16:32:10 -0300

    primeiro exercicio

---
+++ fundamentos/primeiro.go
@@ -0,0 +1,12 @@
+// para que tenha um programa executável, ele tem que rodar no pacote main
+
+package main
+
+// importar pacotes
+
+import "fmt" // IO formatados
+
+func main() {
+	fmt.Print("Primeiro")
+	fmt.Print("Programa!")
+}
