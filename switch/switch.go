7443332978ed7784a47459074369931019a9449b
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-08-23 14:43:50 -0300

    switch

---
+++ switch/switch.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func notaConceito(n float64) string {
+	var nota = int(n)
+	switch nota {
+	case 10:
+		fallthrough
+	case 9:
+		return "A"
+	case 8, 7:
+		return "B"
+	case 6, 5:
+		return "C"
+	case 4, 3:
+		return "D"
+	case 2, 1, 0:
+		return "E"
+	default:
+		return "Nota inválida"
+	}
+}
+func hora() {
+	t := time.Now()
+	switch { // vai pegar o primeiro que for true
+	case t.Hour() < 12:
+		fmt.Println("Bom dia")
+	case t.Hour() < 18:
+		fmt.Println("Boa tarde")
+	default:
+		fmt.Println("Boa noite")
+	}
+}
+
+func main() {
+	fmt.Println(notaConceito(10))
+	hora()
+}
