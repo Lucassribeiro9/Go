f20f415866ef87e52b15380bf814113cb7f1d1de
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-08-26 14:31:06 -0300

    comentando linha

--- arrayslicemap/slicemake.go
+++ arrayslicemap/slicemake.go
@@ -6,4 +6,13 @@ func main() {
 	s := make([]int, 10) // tipo do slice e o número de elementos
 	s[9] = 12
 	fmt.Println(s)
+
+	s = make([]int, 10, 20) // slice tem 10 elementos e array interno tem 20 posições
+	fmt.Println(s, len(s), cap(s))
+
+	s = append(s, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+	fmt.Println(s)
+	fmt.Println(s, len(s), cap(s))
+	s = append(s, 1) // slice cresce e o array interno referencia arrays maiores
+	fmt.Println(s, len(s), cap(s))
 }
