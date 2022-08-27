814c5b709e5861731527ffff334a73dbe282ef4d
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-08-27 17:59:47 -0300

    funcoes basicas

---
+++ funcoes/basicas.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func f1() {
+	fmt.Println("F1")
+}
+func f2(p1 string, p2 string) {
+	fmt.Printf("F2: %s %s\n", p1, p2)
+}
+func f3() string {
+	return "F3"
+}
+func f4(p1, p2 string) string {
+	return fmt.Sprintf("F4: %s %s", p1, p2)
+}
+func f5() (string, string) {
+	return "ret 1", "ret 2"
+}
+
+func main() {
+	f1()
+	f2("p1", "p2")
+	r3, r4 := f3(), f4("a1", "a2")
+	fmt.Println(r3)
+	fmt.Println(r4)
+
+	r51, _ := f5()
+	fmt.Println("F5", r51)
+}
