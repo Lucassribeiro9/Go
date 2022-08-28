46b878b29a70d3166472d8254e1519dbd74999d6
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-08-27 21:00:17 -0300

    variaticas com slice

---
+++ funcoes/variaticas2.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func imprimirAprovados(aprovados ...string) {
+	fmt.Println("Lista de aprovados")
+	for i, aprovado := range aprovados {
+		fmt.Printf("%d) %s\n", i+1, aprovado)
+	}
+}
+func main() {
+	aprovados := []string{"Maria", "Carlos", "Miguel", "Juan"}
+	imprimirAprovados(aprovados...)
+}
