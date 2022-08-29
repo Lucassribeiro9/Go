9b3116dfe97e2e3e2dd971ca7ab9abde5914fe19
Lucassribeiro9 <lucas.devlop94@gmail.com>
2022-08-29 13:20:17 -0300

    recursao simples

---
+++ funcoes/recsimples.go
@@ -0,0 +1,19 @@
+// solução simples
+
+package main
+
+import "fmt"
+
+func fatorial(n uint) uint {
+	switch {
+	case n == 0:
+		return 1
+	default:
+		return n * fatorial(n-1)
+	}
+}
+
+func main() {
+	resultado := fatorial(8)
+	fmt.Println(resultado)
+}
